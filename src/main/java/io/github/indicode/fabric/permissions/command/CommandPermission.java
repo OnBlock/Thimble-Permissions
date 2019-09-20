367939e1e5b276297a8c92fe8b799c53d9ebcc58
Indigo A <indigoa.coding@gmail.com>
2019-09-20 09:21:27 -0700

    No-Save perms

--- src/main/java/io/github/indicode/fabric/permissions/command/CommandPermission.java
+++ src/main/java/io/github/indicode/fabric/permissions/command/CommandPermission.java
@@ -7,17 +7,14 @@ import net.minecraft.server.network.ServerPlayerEntity;
 /**
  * @author Indigo Amann
  */
-public class CommandPermission extends Permission {
+public class CommandPermission extends NoSavePermission {
     public CommandPermission(String command, Permission parent) {
         super(command, parent);
     }
     public CommandPermission(String command) {
         super(command, Thimble.PERMISSIONS.getPermission(Thimble.COMMANDS));
     }
-    @Override
-    public boolean shouldSave() {
-        return false;
-    }
+
     @Override
     public void onStateChanged(ServerPlayerEntity player, boolean hasPerm) {
         player.server.getCommandManager().sendCommandTree(player);
