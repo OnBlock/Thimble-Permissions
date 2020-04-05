18697f116a3c09b502187859b999f7c7ea50ab14
OnBlock-Dev <47112191+OnBlock@users.noreply.github.com>
2020-04-05 15:36:50 +0430

    up.

--- src/main/java/io/github/indicode/fabric/permissions/PermissionLoadHandler.java
+++ src/main/java/io/github/indicode/fabric/permissions/PermissionLoadHandler.java
@@ -24,35 +24,31 @@ public class PermissionLoadHandler extends NBTWorldData {
 
     @Override
     public void onWorldLoad(File file, File file1) {
+        File perms = new File(file, "permissions.json");
+        Map<UUID, PlayerPermissionManager> oldmap = Thimble.PERMISSIONS.permissionMap;
+        Thimble.PERMISSIONS = new PermissionMap();
+        Thimble.LOGGER.debug("Running mod permission injectors");
+        Thimble.permissionWriters.forEach(consumer -> consumer.accept(Thimble.PERMISSIONS, server));
+        Thimble.LOGGER.debug("Loading permissions json");
         try {
-            File perms = new File(file, "permissions.json");
-            Map<UUID, PlayerPermissionManager> oldmap = Thimble.PERMISSIONS.permissionMap;
-            Thimble.PERMISSIONS = new PermissionMap();
-            Thimble.LOGGER.debug("Running mod permission injectors");
-            Thimble.permissionWriters.forEach(consumer -> consumer.accept(Thimble.PERMISSIONS, server));
-            Thimble.LOGGER.debug("Loading permissions json");
-            try {
-                Jankson jankson = JanksonFactory.createJankson();
-                if (!perms.exists()) {
-                    Thimble.LOGGER.warn("permissions.json does not exist");
-                } else {
-                    Thimble.PERMISSIONS.permissionsFromJson(DefaultedJsonObject.of(jankson.load(perms)));
-                }
-
-            } catch (SyntaxError | IOException syntaxError) {
-                Thimble.LOGGER.error("Could not load permissions file", syntaxError);
-                System.exit(1);
-            }
-            Thimble.LOGGER.debug("Loading player permissions");
-            if (reloading) {
-                Thimble.PERMISSIONS.permissionMap = oldmap;
+            Jankson jankson = JanksonFactory.createJankson();
+            if (!perms.exists()) {
+                Thimble.LOGGER.warn("permissions.json does not exist");
             } else {
-                super.onWorldLoad(file, file1);
+                Thimble.PERMISSIONS.permissionsFromJson(DefaultedJsonObject.of(jankson.load(perms)));
             }
-            reloading = true;
-        } catch (Exception e) {
-            e.printStackTrace();
+
+        } catch (SyntaxError | IOException syntaxError) {
+            Thimble.LOGGER.error("Could not load permissions file", syntaxError);
+            System.exit(1);
         }
+        Thimble.LOGGER.debug("Loading player permissions");
+        if (reloading) {
+            Thimble.PERMISSIONS.permissionMap = oldmap;
+        } else {
+            super.onWorldLoad(file, file1);
+        }
+        reloading = true;
     }
 
     @Override
--- src/main/java/io/github/indicode/fabric/permissions/mixin/MinecraftServerMixin.java
+++ src/main/java/io/github/indicode/fabric/permissions/mixin/MinecraftServerMixin.java
@@ -2,26 +2,33 @@ package io.github.indicode.fabric.permissions.mixin;
 
 import com.mojang.authlib.GameProfileRepository;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
-import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import com.mojang.datafixers.DataFixer;
 import io.github.indicode.fabric.permissions.PermissionLoadHandler;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.util.UserCache;
+import net.minecraft.util.Util;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.io.File;
 import java.net.Proxy;
+import java.util.concurrent.Executor;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void saveInstance(LevelStorage.class_5143 arg, Proxy proxy, DataFixer dataFixer, CommandManager commandManager, MinecraftSessionService minecraftSessionService, GameProfileRepository gameProfileRepository, UserCache userCache, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+//    @Inject(method = "<init>", at = @At("RETURN"))
+//    private void saveInstance(LevelStorage.class_5143 arg, Proxy proxy, DataFixer dataFixer, CommandManager commandManager, MinecraftSessionService minecraftSessionService, GameProfileRepository gameProfileRepository, UserCache userCache, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+//        PermissionLoadHandler.server = (MinecraftServer)(Object) this;
+//    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Util;getServerWorkerExecutor()Ljava/util/concurrent/Executor;"))
+    private Executor redirect() {
         PermissionLoadHandler.server = (MinecraftServer)(Object) this;
+        return Util.getServerWorkerExecutor();
     }
 }
